53d9a34161bc51ae1d38c7f5c0026ed99f320eff
板場 <harutch.222@gmail.com>
2019-06-05 12:00:54 +0900

    空間クラス一応完成

--- TOBE！SURAIMU/battle.cpp
+++ TOBE！SURAIMU/battle.cpp
@@ -3,8 +3,25 @@
 
 #include "battle.h"
 
+
+#include "release.h"
+#include "player.h"
+#include "wall.h"
+#include "star_container.h"
+
+
 /*===========================================================================*/
 constexpr unsigned kPlayerMax = 4U;
+constexpr Vector2 kPlayerPosition[kPlayerMax] = {   // プレイヤーの初期位置
+    { 0.0F, 0.0F }, // Player 1
+    { 0.0F, 0.0F }, // Player 2
+    { 0.0F, 0.0F }, // Player 3
+    { 0.0F, 0.0F }, // Player 4
+};
+constexpr float kPlayerJump    = 800.0F;            // プレイヤージャンプ力
+constexpr float kPlayerAddVol  = 0.005F;            // プレイヤー増加量
+constexpr float kPlayerGravity = 5.0F;              // プレイヤー重力
+constexpr float kPlayerSpeed   = 5.0F;              // プレイヤー速さ
 
 
 /*===========================================================================*/
@@ -23,12 +40,32 @@ Battle::~Battle()
 bool Battle::init()
 {
     destroy();
+
+
+    // プレイヤー
+    for( int i = 0; i < kPlayerMax; ++i )
+    {
+        player_.push_back( new Player() );
+
+        if( player_.back()->init(
+            kPlayerPosition[i], kPlayerJump, kPlayerAddVol,
+            kPlayerGravity, kPlayerSpeed
+            ) == false )
+        {
+            return false;
+        }
+    }
+
+    // 壁
+    wall_ = new Wall();
+    wall_->init();
+
+
+    // スター
+    star_container_ = new StarContainer();
+
+
     created_ = true;
-
-
-
-
-
     return true;
 }
 
@@ -38,6 +75,19 @@ void Battle::destroy()
 {
     if( created_ == false ) { return; }
     created_ = false;
+
+
+    // 星たちの開放
+    star_container_->destroy();     safe_delete(star_container_);
+
+    // 壁の開放
+    wall_->destroy();               safe_delete(wall_);
+
+    // プレイヤーたちを開放
+    for( auto player : player_ )
+    {
+        player->destroy();          safe_delete(player);
+    }
 
 }
 
