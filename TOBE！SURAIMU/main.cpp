2dca3f0ed58701801889718fb3cf36b2c5ce26de
山田晴久 <hisahisa6.25@gmail.com>
2019-09-05 16:33:43 +0900

    マウスカーソルの削除

--- TOBE！SURAIMU/main.cpp
+++ TOBE！SURAIMU/main.cpp
@@ -21,7 +21,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	//_CrtSetBreakAlloc(751);
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #else
-	//for (; ShowCursor(false) < 0;);
+	for (int cusur = ShowCursor(false); cusur > -1; ShowCursor(false));
 
 #endif
 
